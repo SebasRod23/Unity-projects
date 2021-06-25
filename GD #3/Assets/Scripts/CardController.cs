f172adfde9a9f54b8058144a362cccf65c706bb4
SebasRod23 <sebastian_rodriguezg@yahoo.com>
2021-06-25 17:30:54 -0500

    GD #3

---
+++ GD #3/Assets/Scripts/CardController.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CardController : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public SpriteRenderer TheSpriteRenderer;
+    public Sprite Front;
+    public Sprite Back;
+    public bool isFront=false;
+    public int CardIndex;
+    public bool found = false;
+    void Start()
+    {
+        ShowBack();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (found) gameObject.SetActive(false);
+    }
+
+    private void OnMouseDown()
+    {
+        Debug.Log("CardIndex: " + CardIndex);
+        if (!isFront) ShowFront();
+        else ShowBack();
+    }
+    public void ShowFront()
+    {
+        TheSpriteRenderer.sprite = Front;
+        isFront = true;
+    }
+    public void ShowBack()
+    {
+        TheSpriteRenderer.sprite = Back;
+        isFront = false;
+    }
+}
---
+++ GD #3/Assets/Scripts/GameManager.cs
@@ -0,0 +1,66 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public Sprite[] Sprites;
+    public CardController[] Cards;
+    public int CardsClicked;
+    private int[] indexs=new int[6];
+    private int[] states=new int[3];
+    void Start()
+    {
+        //Asigna el valor de los tres pares
+        indexs[0] = Random.Range(0, 7);
+        indexs[1] = indexs[0];
+        indexs[2] = Random.Range(0, 7);
+        indexs[3] = indexs[2];
+        indexs[4] = Random.Range(0, 7);
+        indexs[5] = indexs[4];
+        for (int i = 0; i < 6; i++)
+        {
+            int r = Random.Range(i, 6);
+            int t = indexs[r];
+            indexs[r] = indexs[i];
+            indexs[i] = t;
+        }
+        for (int i = 0; i < 6; i++)
+        {
+            Cards[i].Front = Sprites[indexs[i]];
+            Cards[i].CardIndex = indexs[i];
+        }
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        List<CardController> CardsSelected = new List<CardController>();
+        foreach(CardController card in Cards)
+        {
+            if (card.isFront&&!card.found) CardsSelected.Add(card);
+        }
+        //Debug.Log("Cards Selected: "+CardsSelected.Count);
+        if (CardsSelected.Count == 2)
+        {
+            //Checar si son iguales
+            if (CardsSelected[0].CardIndex == CardsSelected[1].CardIndex)
+            {
+                Debug.Log("They match");
+                CardsSelected[0].found = true;
+                CardsSelected[1].found = true;
+            }
+            else
+            {
+                Debug.Log("They do not match");
+                foreach (CardController card in Cards)
+                {
+                    card.isFront = false;
+                    card.ShowBack();
+                }
+            }
+        }
+    }
+}
